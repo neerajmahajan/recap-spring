3787e19b97ed0aead46e6157d78c4b347464a6e6
Neeraj Mahajan <neeraj.mahajan@specsavers.com>
2018-12-22 22:20:01 +0000

    Added factory-method and factory-bean example

--- spring-basic/src/main/java/learn/spring/configuration/ModelConfiguration.java
+++ spring-basic/src/main/java/learn/spring/configuration/ModelConfiguration.java
@@ -2,13 +2,15 @@ package learn.spring.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import learn.spring.model.LoginModel;
 import learn.spring.model.Model;
 
 @Configuration
 public class ModelConfiguration {
-	@Bean
+	
+	@Bean @Scope("prototype") // By default scope is singleton
 	public Model loginModel(){
 		return new LoginModel("loginModel");
 	}
